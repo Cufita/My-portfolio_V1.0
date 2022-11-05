c9f7d1de05cfd237db1f1f205058a542b913f018
Cufita <facuamores@gmail.com>
2022-11-05 12:30:06 -0300

    Adding dark mode to my Portfolio

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-let lightDarkMode = document.getElementById(lightDarkMode);
+let icon = document.getElementById("lightDarkMode");
+let body = document.getElementById("body");
+let header = document.getElementById("header");
 
-lightDarkMode.addEventListener("click", () => {});
+//Change the color of the WebPage------------------------------------------------------------//
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "./img/moon.png";
+  } else {
+    icon.src = "./img/sunLogo.png";
+  }
+};
+
+body.addEventListener("scroll", (e) => {
+  e.preventDefault;
+});
