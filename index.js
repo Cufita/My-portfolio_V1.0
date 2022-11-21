87637b95604b2b803dc4753ee5b3953c2eab465b
Cufita <facuamores@gmail.com>
2022-11-21 17:53:01 -0300

    Finishing Portfolio,left do it Responsive

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let icon = document.getElementById("lightDarkMode");
+let body = document.getElementById("body");
+let header = document.getElementById("header");
+
+//Change the color of the WebPage------------------------------------------------------------//
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "./img/moon.png";
+  } else {
+    icon.src = "./img/sunLogo.png";
+  }
+};
+
+body.addEventListener("", () => {});
+
+//Library for scroll animations---------------------------------------------------------------//
+
+AOS.init({
+  duration:800
+});
+
+
